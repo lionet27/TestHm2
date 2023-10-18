ce3fdd907fa181c25f860e8e5014347b088a012d
Soboleva Olga <osoboleva27@gmail.com>
2023-10-18 23:48:33 +0300

    add void PrintNotebooks

--- FilterNotebook.java
+++ FilterNotebook.java
@@ -1,3 +1,20 @@
+
+//     Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
+//     Создать множество ноутбуков.
+//     Написать метод, который будет запрашивать у пользователя критерий фильтрации и выведет ноутбуки, отвечающие фильтру. 
+// Критерии фильтрации можно хранить в Map. минимум 5 
+
+import java.util.ArrayList;
+// import java.util.List;
+import java.util.Scanner;
+
+
 public class FilterNotebook {
+    public static void PrintNotebooks(ArrayList<Notebook> notebooks){
+        for (int i=0; i<notebooks.size();i++) {
+            System.out.println(notebooks.get(i).toString());
+            System.out.println();
+        }
+    }
     
 }
