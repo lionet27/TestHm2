81f51348dcea51e4a56b8f7a7657f58e8c36edaa
Soboleva Olga <osoboleva27@gmail.com>
2023-10-19 22:36:13 +0300

    Revert "add notebook in Main" - task1
    This reverts commit 359af6835cd82c859347365f7f5c137c55675277.

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ public class Main {
         notebooks.add(new Notebook(16, 1, "MacOS", "black"));
         notebooks.add(new Notebook(32, 2, "Linux", "grey"));
         notebooks.add(new Notebook(8, 512, "Windows", "white"));
-        notebooks.add(new Notebook(32, 512, "Windows", "black"));
 
         ArrayList<Notebook> filterNotebooks=FilterNotebook.FilterNotebooks(notebooks);
         FilterNotebook.PrintNotebooks(filterNotebooks);
