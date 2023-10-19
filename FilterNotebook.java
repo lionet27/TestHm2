e6b46c549a930532b8df785e32265d97320794d8
Soboleva Olga <osoboleva27@gmail.com>
2023-10-19 21:18:26 +0300

    add Main

--- FilterNotebook.java
+++ FilterNotebook.java
@@ -16,5 +16,101 @@ public class FilterNotebook {
             System.out.println();
         }
     }
+
+    public static ArrayList<Notebook> FilterNotebooks (ArrayList<Notebook> notebooks){
+        ArrayList<Notebook>filterNotebooks=new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Выберете по какому параметру Вы хотите отфильтровать ноутбуки:1-ОЗУ; 2 -Объем ЖД; 3- Операционная система;4-цвет");
+            String choice=sc.nextLine();
+            boolean flag=false;
+            if (choice.equals("1")){
+                
+                while(flag==false){
+                    System.out.println("Введите размер ОЗУ(Гб):8; 16; 32");
+                    int ozy=sc.nextInt();                     
+                    for (int i=0;i<notebooks.size();i++){
+                        if (ozy==(notebooks.get(i).OZY)){
+                            filterNotebooks.add(notebooks.get(i));
+                            flag=true;
+                        }   
+                    }
+                }
+            }else if(choice.equals("2")){
+                    while(flag==false){
+                        System.out.println("Введите обьем ЖД(512Мб,1Tб,2Tб): 512; 1; 2 ");
+                        int hdcapacity=sc.nextInt();
+                        for (int i=0;i<notebooks.size();i++){
+                            if (hdcapacity==(notebooks.get(i).HDcapacity)){
+                                filterNotebooks.add(notebooks.get(i));
+                                flag=true;
+                            }   
+                        }
+                    }
+                
+            }else if(choice.equals("3")){
+                    while(flag==false){
+                        System.out.println("Выберете вариант операционной системы:1-Windows; 2-Linux;3- MacOS");
+                        String op=sc.nextLine();
+                        if (op.equals("1")){
+                            for (int i=0;i<notebooks.size();i++){
+                                if (notebooks.get(i).op.equals("Windows")){
+                                    filterNotebooks.add(notebooks.get(i));
+                                    flag=true;
+                                }   
+                            }
+                        }else if(op.equals("2")){
+                            for (int i=0;i<notebooks.size();i++){
+                                if (notebooks.get(i).op.equals("Linux")){
+                                    filterNotebooks.add(notebooks.get(i));
+                                    flag=true;
+                                }                   
+                            }
+                        }else if(op.equals("3")){
+                            for (int i=0;i<notebooks.size();i++){
+                                if (notebooks.get(i).op.equals("MacOS")){
+                                    filterNotebooks.add(notebooks.get(i));
+                                    flag=true;
+                                }
+                            }
+                        }else{
+                            System.out.println("неправильно ввели число");
+                        }       
+                    }
+                
+            }else if(choice.equals("4")){
+                while(flag==false){
+                    System.out.println("Выберете вариант операционной системы:1-white; 2-grey;3- black");
+                    String colour=sc.nextLine();
+                    if (colour.equals("1")){
+                        for (int i=0;i<notebooks.size();i++){
+                            if (notebooks.get(i).colour.equals("white")){
+                                filterNotebooks.add(notebooks.get(i));
+                                flag=true;
+                            }   
+                        }
+                    }else if(colour.equals("2")){
+                        for (int i=0;i<notebooks.size();i++){
+                            if (notebooks.get(i).colour.equals("grey")){
+                                filterNotebooks.add(notebooks.get(i));
+                                flag=true;
+                            }                       
+                        }
+                    }else if(colour.equals("3")){
+                        for (int i=0;i<notebooks.size();i++){
+                            if (notebooks.get(i).colour.equals("black")){
+                                filterNotebooks.add(notebooks.get(i));
+                                flag=true;
+                            }  
+                        }
+                    }else{
+                        System.out.println("неправильно ввели число");
+                    }       
+                }
+            }else{
+                System.out.println("неправильно ввели число");
+            }
+        sc.close();
+        return filterNotebooks;
+    }
     
 }
---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Notebook>notebooks=new ArrayList<>();
+
+        notebooks.add(new Notebook(32, 512, "Windows", "white"));
+        notebooks.add(new Notebook(8, 1, "Linux", "grey"));
+        notebooks.add(new Notebook(16, 1, "MacOS", "black"));
+        notebooks.add(new Notebook(32, 2, "Linux", "grey"));
+        notebooks.add(new Notebook(8, 512, "Windows", "white"));
+
+        ArrayList<Notebook> filterNotebooks=FilterNotebook.FilterNotebooks(notebooks);
+        FilterNotebook.PrintNotebooks(filterNotebooks);
+    }
+    
+}
